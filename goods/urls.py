f95274108630a1977b8fbe72e359ddd823aace4d
SergeyFoma <asdfg2105c@gmail.com>
2025-10-09 14:21:53 +0500

    slug and id

--- goods/urls.py
+++ goods/urls.py
@@ -5,5 +5,6 @@ app_name="goods"
 
 urlpatterns = [
     path('catalog/', views.catalog, name="catalog"),
+    #path('product/<int:product_id>/', views.product, name="product"),
     path('product/<slug:product_slug>/', views.product, name="product"),
 ]
--- goods/views.py
+++ goods/views.py
@@ -11,6 +11,14 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
+# def product(request, product_slug=False, product_id=False):
+#     if product_id:
+#         product = Products.objects.get(id=product_id)
+#     else:
+#         product = Products.objects.get(slug=product_slug)
+#     context = {"product": product}
+#     return render(request, "goods/product.html", context)
+
 def product(request, product_slug):
     product = Products.objects.get(slug=product_slug)
     context = {"product": product}
