f61336b8cb5d349a74850d2d6a33e53b409a8baf
SergeyFoma <asdfg2105c@gmail.com>
2025-10-09 15:29:48 +0500

    paginator начало

--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from goods.models import Products
 from django.shortcuts import get_list_or_404
-
+from django.core.paginator import Paginator
 
 def catalog(request, category_slug):
-    if category_slug == 'vse-tovary':
+    if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 2)
+    current_page = paginator.page(2)
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        #"goods": goods,
+        "goods":current_page,
     }
     return render(request, "goods/catalog.html", context)
 
