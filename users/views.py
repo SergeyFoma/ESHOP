1f06bb965c13918db2b495cb6e779c83d2bc6811
SergeyFoma <asdfg2105c@gmail.com>
2025-10-23 13:49:27 +0500

    Регистрация user

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request) -> HttpResponseRedirect | HttpResponse:
     if request.method == 'POST':
@@ -26,9 +26,17 @@ def login(request) -> HttpResponseRedirect | HttpResponse:
     return render(request, "users/login.html", context)
 
 def registration(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
     title = "Регистрация"
     context= {
-        "title":title,   
+        "title":title,
+        "form":form,   
     }
     return render(request, "users/registration.html", context)
 
