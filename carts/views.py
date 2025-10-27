70ff9e3724cbeff2b87efd6df01a3c98525c9fbb
SergeyFoma <asdfg2105c@gmail.com>
2025-10-27 11:23:05 +0500

    model Cart

--- carts/views.py
+++ carts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
-def cart_add(request, product_id):
+def cart_add(request, product_slug):
     context = {
 
     }
     return render(request, "carts/cart_add.html", context)
 
-def cart_change(request, product_id):
+def cart_change(request, product_slug):
     context = {
 
     }
     return render(request, "carts/cart_change.html", context)
 
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     context = {
 
     }
